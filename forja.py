646b63ce9bef758c956012c169cc83ea4bdba101
lemoniac <aldomon@gmail.com>
2016-05-17 23:15:09 +0100

    Parse messages&fields in the .def file Implement partially __str__ Print the protocol and the packet definitions after loading them.

--- forja.py
+++ forja.py
@@ -13,23 +13,61 @@ class Field:
 		self.ignore = ignore
 		self.value = value
 
+	def __str__(self):
+		s = self.name
+
+		if self.value != None:
+			s += " = " + self.value
+
+		return s
+
 class Message:
 	def __init__(self, name, fields):
 		self.name = name
 		self.fields = fields
 
+	def __str__(self):
+		s = "Message: " + self.name + "\n"
+
+		for f in self.fields:
+			s += "    " + str(f) + "\n"
+
+		return s
+
 class Packet:
 	def __init__(self, messages = []):
 		self.messages = messages
 
+	def __str__(self):
+		s = "Packet:\n"
+		for m in self.messages:
+			s += "    Message " + m[0] + "\n"
+			for f in m[1]:
+				s += "        " + f[0] + " = " + f[1] + "\n"
+		return s
+
 class Protocol:
 	def __init__(self):
-		self.messages = []
+		self.messages = {}
 		self.endianness = "littleendian"
+
+	def __str__(self):
+		s = self.endianness + "\n"
+
+		for n, m in self.messages.iteritems():
+			s += str(m) + "\n"
+
+		return s
+
+
+	def add_message(self, message):
+		self.messages[message.name] = message
 
 class Loader:
 	def __init__(self, filename):
 		self.lexer = shlex.shlex(open(filename, "rt"))
+
+		self.protocol = Protocol()
 
 		self.load()
 
@@ -46,7 +84,6 @@ class Loader:
 	def load(self):
 		token = self.get_token()
 		while len(token) > 0:
-			print token
 			try:
 				token = self.get_token()
 			except Exception:
@@ -54,22 +91,24 @@ class Loader:
 			if token == "set":
 				self.parse_set()
 			elif token == "struct" or token == "message":
-				self.parse_struct()
+				self.protocol.add_message( self.parse_struct() )
 
 	def parse_set(self):
 		token = self.get_token()
-		print token
 		if token == "little_endian":
 			self.expect(";")
 
 	def parse_struct(self):
 		name = self.get_token()
+		fields = []
 		self.expect("{")
 		token = self.get_token()
 		while token != "}":
-			field = self.parse_field(token)
-			print field
+			fields.append( self.parse_field(token) )
+
 			token = self.get_token()
+
+		return Message(name, fields)
 
 	def parse_list(self):
 		l = []
@@ -110,7 +149,6 @@ class Loader:
 				self.expect(";")
 			elif token == "valid":
 				valid = self.parse_list()
-				print valid
 				self.expect(";")
 			return (field_type, size, field_name)
 
@@ -134,7 +172,6 @@ class DefLoader:
 	def load(self):
 		token = self.get_token()
 		while len(token) > 0:
-			print token
 			try:
 				token = self.get_token()
 			except Exception:
@@ -172,9 +209,21 @@ class DefLoader:
 		self.expect("{")
 		token = self.get_token()
 		while token != "}":
+			field_name = token
+			self.expect("=")
+			field_value = self.get_token()
 			token = self.get_token()
+			if token == ",":
+				token = self.get_token()
 
-		return Message(name, fields)
+			fields.append( (field_name, field_value) )
+
+		return (name, fields)
 
 loader = Loader(sys.argv[1])
 def_loader = DefLoader(sys.argv[2])
+
+print loader.protocol
+print "\n---\n"
+for p in def_loader.packets:
+	print p
